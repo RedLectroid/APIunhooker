8573875e2498bc78f699ea47b7a4337b184ca22b
ShellStorm <shotokanb@gmail.com>
2020-10-13 19:42:28 -0400

    Update APIunhook.cpp
    fixed an error in writing to the bytes.

--- APIunhook.cpp
+++ APIunhook.cpp
@@ -29,48 +29,36 @@ DWORD RvaToOffset(IMAGE_NT_HEADERS * nth, DWORD RVA)
 	return 0;
 }
 
-void unhookAPI(wstring temp, const char *functionName) {
-	
-	LPCWSTR dllName;
-	dllName = temp.c_str();
-
-	HMODULE lib = LoadLibrary(dllName);
-
-	BYTE assemblyBytes[4] = {};
+void unhookAPI(const char* functionName) {
+	HMODULE lib = LoadLibrary(L"C:\\Windows\\System32\\ntdll.dll");
+	BYTE assemblyBytes[5] = {};
 	char assemblyString[60];
+	int buffSize;
 
 	if (lib) {
 		DWORD base = (DWORD)lib;
-		void *fa = GetProcAddress(lib, functionName);
+		void* fa = GetProcAddress(lib, functionName);
 		if (fa) {
-			IMAGE_DOS_HEADER * dos = (IMAGE_DOS_HEADER *)lib;
-			IMAGE_NT_HEADERS * nth = (IMAGE_NT_HEADERS *)(base + dos->e_lfanew);
-			BYTE *read = (BYTE *)fa;
+			IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)lib;
+			IMAGE_NT_HEADERS* nth = (IMAGE_NT_HEADERS*)(base + dos->e_lfanew);
+			BYTE* read = (BYTE*)fa;
 			for (int i = 0; i < 5; i++) {
 				assemblyBytes[i] = read[i];
 			}
-			
 		}
 		else
 			printf("Function not found!\n");
 	}
 	else
 		printf("Error loading library!\n");
-	
-	sprintf_s(assemblyString, "\\x%02x\\x%02x\\x%02x\\x%02x\\x%02x", assemblyBytes[0], assemblyBytes[1], assemblyBytes[2], assemblyBytes[3], assemblyBytes[4]);
-	printf("First 5 bytes of %s are %s\n", functionName, assemblyString );
-
-	WriteProcessMemory(GetCurrentProcess(), GetProcAddress(GetModuleHandle(dllName), functionName), assemblyString, 5, NULL);
-
+	WriteProcessMemory(GetCurrentProcess(), GetProcAddress(GetModuleHandle(L"ntdll"), functionName), (LPCVOID)assemblyBytes, 5, NULL);
 	FreeLibrary(lib);
-
-
-}
+	}
 
 int main() {
 
 	
-	unhookAPI(L"C:\\Windows\\System32\\ntdll.dll", "NtReadVirtualMemory");
+	unhookAPI("NtReadVirtualMemory");
 
 	return 0;
 }
