e354b9f9202225713a63184ba1937921e6abd051
ShellStorm <shotokanb@gmail.com>
2020-10-14 10:47:46 -0400

    Update APIunhook.cpp

--- APIunhook.cpp
+++ APIunhook.cpp
@@ -8,15 +8,13 @@ using namespace std;
 #pragma comment(lib,"imagehlp")
 
 void unhookAPI(const char* functionName) {
+
 	HMODULE lib = LoadLibrary(L"C:\\Windows\\System32\\ntdll.dll");
 	BYTE assemblyBytes[5] = {};
-
+	
 	if (lib) {
-		DWORD base = (DWORD)lib;
 		void* fa = GetProcAddress(lib, functionName);
 		if (fa) {
-			IMAGE_DOS_HEADER* dos = (IMAGE_DOS_HEADER*)lib;
-			IMAGE_NT_HEADERS* nth = (IMAGE_NT_HEADERS*)(base + dos->e_lfanew);
 			BYTE* read = (BYTE*)fa;
 			for (int i = 0; i < 5; i++) {
 				assemblyBytes[i] = read[i];
